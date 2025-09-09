9f866954c11f4e40963040e257967bfe6eefaa77
NeilyZitouni <nn_zitouni@esi.dz>
2025-09-10 01:07:16 +0200

    added crud operations on the products

--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,113 @@
+const { json } = require("express");
+const Product = require("../models/products");
+const { StatusCodes } = require("http-status-codes");
+
 const getAllproducts = async (req, res) => {
-  res.send("get all products");
+  const products = await Product.find({});
+  if (products.length === 0) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `no product with id : ${id}` });
+  }
+  res.status(StatusCodes.OK).json({
+    nbHits: products.length,
+    products,
+  });
+};
+
+const getAllProductsCreatedByUser = async (req, res) => {
+  const { userId } = req.user;
+  if (!userId) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ msg: "User not authenticated" });
+  }
+  const products = await Product.find({ createdBy: userId });
+  if (products.length === 0) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `no product with id : ${id}` });
+  }
+  if (!products) {
+    return res
+      .status(StatusCodes.OK)
+      .json({ msg: "you havent created anything yet" });
+  }
+  res.status(StatusCodes.OK).json({ nbHits: products.length, products });
 };
 
 const getProduct = async (req, res) => {
-  res.send("get all products");
+  const { id } = req.params;
+  if (!id) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "please provide the product's id" });
+  }
+  const product = await Product.findById(id);
+  if (!product) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `no product with id : ${id}` });
+  }
+  res.status(StatusCodes.OK).json({ product });
 };
 
 const createProduct = async (req, res) => {
-  res.send("get all products");
+  req.body.createdBy = req.user.userId;
+  const newProduct = await Product.create(req.body);
+  res.status(StatusCodes.CREATED).json({ newProduct });
 };
 
 const updateProduct = async (req, res) => {
-  res.send("get all products");
+  const {
+    body: { name, description, company, category, price, priceCategory },
+    user: { userId },
+    params: { id: productId },
+  } = req;
+  if (
+    name === "" ||
+    company === "" ||
+    category === "" ||
+    price === "" ||
+    priceCategory === ""
+  ) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json("name, company, category, price and priceCategory cannot be empty");
+  }
+  const newproduct = await Product.findOneAndUpdate(
+    { _id: productId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  if (!newproduct) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `the product you are looking for doesn't exists` });
+  }
+  res.status(StatusCodes.OK).json({ newproduct });
 };
 
 const deleteProduct = async (req, res) => {
-  res.send("get all products");
+  const {
+    params: { id: productId },
+    user: { userId },
+  } = req;
+  if (!productId) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "please enter a product id" });
+  }
+  const result = await Product.findOneAndDelete({
+    _id: productId,
+    createdBy: userId,
+  });
+  if (!result) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `no product with id : ${productId}` });
+  }
+  res.status(StatusCodes.OK).json({ msg: "deleted the product successfully" });
 };
 
 module.exports = {
@@ -24,4 +116,5 @@ module.exports = {
   createProduct,
   updateProduct,
   deleteProduct,
+  getAllProductsCreatedByUser,
 };
--- routes/products.js
+++ routes/products.js
@@ -7,9 +7,11 @@ const {
   createProduct,
   updateProduct,
   deleteProduct,
+  getAllProductsCreatedByUser,
 } = require("../controllers/products");
 
 router.route("/").get(getAllproducts).post(createProduct);
+router.route("/created").get(getAllProductsCreatedByUser);
 
 router.route("/:id").get(getProduct).patch(updateProduct).delete(deleteProduct);
 
